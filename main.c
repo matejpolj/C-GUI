ee675be793fa358660b33cb955b14774d52bd026
matejpolj <42142798+matejpolj@users.noreply.github.com>
2023-08-25 16:12:52 +0200

    Added lots of text on screen!

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ static void click1_cb(GtkButton *btn) {
 
     s = gtk_button_get_label(btn);
     if (g_strcmp0(s, "Hello.") == 0) {
-        gtk_button_set_label(btn, "Goob-bye.");
+        gtk_button_set_label(btn, "Good-bye.");
     } else {
         gtk_button_set_label(btn, "Hello.");
     }
@@ -66,8 +66,41 @@ static void app_activate1(GApplication *app) {
     gtk_box_append(GTK_BOX(box), btn2);
 
     gtk_window_present(GTK_WINDOW(win));
+}
 
+static void app_activate2(GApplication *app) {
+    GtkWidget *win;
+    GtkWidget *tv;
+    GtkWidget *scr;
+    GtkTextBuffer *tb;
+    gchar *text;
 
+    text = "Once upon a time, there was an old man who was called Taketori-no-Okina. "
+           "It is a japanese word that means a man whose work is making bamboo baskets.\n"
+           "One day, he went into a hill and found a shining bamboo. "
+           "\"What a mysterious bamboo it is!,\" he said. "
+           "He cut it, then there was a small cute baby girl in it. "
+           "The girl was shining faintly. "
+           "He thought this baby girl is a gift from Heaven and took her home.\n"
+           "His wife was surprised at his story. "
+           "They were very happy because they had no children. ";
+
+    win = gtk_application_window_new((GTK_APPLICATION(app)));
+    gtk_window_set_title(GTK_WINDOW(win), "Taketori");
+    gtk_window_set_default_size(GTK_WINDOW(win), 400, 300);
+
+    scr = gtk_scrolled_window_new();
+    gtk_window_set_child(GTK_WINDOW(win), scr);
+
+    tv = gtk_text_view_new();
+    tb = gtk_text_view_get_buffer(GTK_TEXT_VIEW(tv));
+    gtk_text_buffer_set_text(tb, text, -1);
+    gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(tv), GTK_WRAP_WORD_CHAR);
+
+    //gtk_window_set_child(GTK_WINDOW(win), tv);
+    gtk_scrolled_window_set_child(GTK_SCROLLED_WINDOW(scr), tv);
+
+    gtk_window_present(GTK_WINDOW(win));
 }
 
 int main(int argc, char* argv[]) {
@@ -75,7 +108,7 @@ int main(int argc, char* argv[]) {
     int stat;
 
     app = gtk_application_new("com.github.ToshioCp.pr1", G_APPLICATION_DEFAULT_FLAGS);
-    g_signal_connect(app, "activate", G_CALLBACK(app_activate1), NULL);
+    g_signal_connect(app, "activate", G_CALLBACK(app_activate2), NULL);
     stat = g_application_run(G_APPLICATION(app), argc, argv);
     g_object_unref(app);
     return stat;
